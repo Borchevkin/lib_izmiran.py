3e76b2e76882971249db547ecf25c0c122e83bb1
Danil Borchevkin <danil.borchevkin@gmail.com>
2024-10-15 02:56:10 +0300

    WIP - Writing tests for DAT file class

--- src/datfile.py
+++ src/datfile.py
@@ -10,20 +10,36 @@ class NoColumnsNameError(DatFileError):
 class NoDataInObjectError(DatFileError):
     pass
 
+class NoColumnError(DatFileError):
+    pass
+
+class InvalidColumnError(DatFileError):
+    pass
+
+
 class DatFile(object):
     """
     Class for represent *.dat files
     """
 
     def __init__(self) -> None:
+        '''
+        DatFile constructor
+        '''
         self.columns_names = []
         self.data_list_of_records = []
-        self.data_list_of_columns = []
 
     def __del__(self) -> None:
+        '''
+        DatFile deconstruction
+        '''
         pass # TODO
 
-    def parse_record_from_line(self, line: str, reject_columns: list=[]) -> list:
+    @staticmethod
+    def parse_record_from_line(line: str, reject_columns: list=[]) -> list:
+        '''
+        Parse record from line.
+        '''
         line_list = []
 
         for idx, val in enumerate(line.split('\t')):
@@ -31,6 +47,13 @@ class DatFile(object):
                 line_list.append(val.replace("\n", ""))
         
         return line_list
+    
+    @staticmethod
+    def parse_string_val_to_float(val: str) -> float:
+        '''
+        Convert string value to float falue
+        '''
+        return float(val)
 
     def parse_from(self, filepath: str, has_header: bool=False, reject_columns: list=[]) -> None:
         '''
@@ -47,32 +70,66 @@ class DatFile(object):
 
         for idx, line in enumerate(lines):
             if has_header and idx == 0:
-                column_names = self.parse_record_from_line(line, reject_columns)
+                column_names = DatFile.parse_record_from_line(line, reject_columns)
                 self.set_columns_names(column_names)
 
-            line_list = self.parse_record_from_line(line, reject_columns)
+            line_list = DatFile.parse_record_from_line(line, reject_columns)
             self.data_list_of_records.append(line_list)
 
     def get_columns_count(self) -> int:
+        '''
+        Get columns count.
+        '''
         if len(self.data_list_of_records) == 0:
-            raise NoDataInObjectError("No data in object")
+            return 0
+        
         return len(self.data_list_of_records[0])
- 
 
     def get_columns_names(self) -> list:
+        '''
+        Get columns names.
+        '''
         if len(self.columns_names) == 0:
             raise NoColumnsNameError("No columns names")
         return self.columns_names.copy()
 
     def set_columns_names(self, names: list) -> None:
+        '''
+        Set column names.
+        '''
+        if self.get_columns_count() != len(names):
+            raise InvalidColumnError("Invalid column count")
+
         self.columns_names = names.copy()
 
     def get_records_count(self) -> int:
+        '''
+        Get records count.
+        '''
         return len(self.data_list_of_records)
 
     def get_records(self) -> list:
+        '''
+        Get all records.
+        '''
         return self.data_list_of_records
 
     def get_record(self, index: int) -> list:
+        '''
+        Get record by index.
+        '''
         if index >= len(self.data_list_of_records):
             raise RecordIndexError("Index out of range")
+        
+        return self.data_list_of_records[index].copy()
+    
+    def add_record(self, record: list) -> None:
+        if self.get_columns_count() == 0:
+            pass
+        else:
+            if len(record) != self.get_columns_count():
+                raise InvalidColumnError("Invalid column count")
+        
+        self.data_list_of_records.append(record.copy())
+
+
--- test/unit_py/test_unit_datfile.py
+++ test/unit_py/test_unit_datfile.py
@@ -1,4 +1,47 @@
 import pytest
+from datfile import *
 
-def test_unit_start():
-    assert 1 == 0, "just simple testing"
+def test_unit_parse_line():
+    result = DatFile.parse_record_from_line("0\t1\t2")
+    assert ["0","1","2"] == result, "Parsing failed"
+
+def test_unit_parse_value():
+    result = DatFile.parse_string_val_to_float("1")
+    assert 1.0 == result, "Parsing failed"
+
+def test_add_records():
+    datfile = DatFile()
+    datfile.add_record(["0","1","2"])
+    assert True, "Adding records failed"
+
+def test_get_columns_count():
+    '''
+    Get columns count.
+    '''
+    datfile = DatFile()
+    ret = datfile.get_columns_count()
+    assert 0 == ret, "Wrong columns count returned"
+    
+    datfile.add_record(["0","1","2"])
+    ret = datfile.get_columns_count()
+    assert 3 == ret, "Wrong columns count returned"
+
+def test_get_columns_name():
+    assert False, "FInish it"
+
+def test_get_record_count():
+    datfile = DatFile()
+    ret = datfile.get_records_count()
+    assert 0 == ret, "Wrong records count returned"
+
+    datfile.add_record(["0","1","2"])
+    ret = datfile.get_records_count()
+    assert 1 == ret, "Wrong records count returned"
+
+    datfile.add_record(["0","1","2"])
+    ret = datfile.get_records_count()
+    assert 2 == ret, "Wrong records count returned"
+
+    datfile.add_record(["0","1","2"])
+    ret = datfile.get_records_count()
+    assert 3 == ret, "Wrong records count returned"
